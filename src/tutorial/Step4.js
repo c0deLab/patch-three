d154945b350cb5db6f08be2536f3fa9586ce3ed0
Scott Donaldson <scott.p.donaldson@gmail.com>
2017-09-18 13:42:08 -0400

    add zoomToFit to tutorial

--- src/tutorial/Step4.js
+++ src/tutorial/Step4.js
@@ -13,6 +13,7 @@ export default class Step extends Component {
 				<h1>Camera: Zoom</h1>
 				<p>You might want to zoom the camera out, if the surface has grown too big to fit on the screen, or zoom in for a better look at an interesting area.</p>
 				<p>Pressing <b>ZOOM</b> on the keypad. Then, rotate the control knob to move the camera up and down.</p>
+				<p>If you ever zoom too far in or out, press <b>FIT</b> to fit the surface within the camera view.</p>
 				<p>Press <b>TUT</b> to try it out now, then press <b>TUT</b> again when you're ready to continue.</p>
 			</div>
 		);
